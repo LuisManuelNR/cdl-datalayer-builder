13a1b4ffda6debaf330c33ca6a6d51f5c0a24bfa
Chasi <luismanuelchazy@gmail.com>
2022-05-06 12:55:22 +0200

    fist commit

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { CApp } from 'chasi-lib'
+  import Nav from 'src/components/Nav.svelte'
+</script>
+
+<CApp>
+  <Nav />
+</CApp>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { CNav } from 'chasi-lib'
+</script>
+
+<CNav>
+  <h4>Events</h4>
+</CNav>
